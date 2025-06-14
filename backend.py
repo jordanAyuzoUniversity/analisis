956e6e6bb8dd2daabe7b84d2991d6133cc57e88e
jordanAyuzoUniversity <aupj021014@gs.utm.mx>
2025-06-13 18:26:14 -0600

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,9 +1,21 @@
-import joblib
 import streamlit as st
+import os
+import joblib
+import gdown
 
-@st.cache_resource
+MODEL_PATH = "sentiment_stacking_model.pkl"
+MODEL_ID = "13LDynQLrKQQwUgkkZcVdV6quRocrxAKO"
+MODEL_URL = f"https://drive.google.com/uc?id={MODEL_ID}"
+
+def download_model():
+    if not os.path.exists(MODEL_PATH):
+        print("🔽 Descargando modelo desde Google Drive...")
+        gdown.download(MODEL_URL, MODEL_PATH, quiet=False)
+
+@st.cache_resource  # ✅ Esto es recomendable
 def load_model():
-    return joblib.load("sentiment_stacking_model.pkl")
+    download_model()
+    return joblib.load(MODEL_PATH)
 
 def predict_sentiment(model, text):
     return model.predict([text])[0]
